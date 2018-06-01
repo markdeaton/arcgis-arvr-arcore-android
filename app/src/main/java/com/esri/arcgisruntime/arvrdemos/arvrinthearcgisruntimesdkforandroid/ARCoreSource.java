53041d8ab355d54a75cecc4a59e65852325c4c2e
Mark Deaton <medeaton@hotmail.com>
2018-06-01 10:15:43 -0700

    * Code cleanup for ARCoreDataSource component

--- app/src/main/java/com/esri/arcgisruntime/arvrdemos/arvrinthearcgisruntimesdkforandroid/ARCoreSource.java
+++ app/src/main/java/com/esri/arcgisruntime/arvrdemos/arvrinthearcgisruntimesdkforandroid/ARCoreSource.java
@@ -16,6 +16,9 @@ import com.google.ar.sceneform.math.Quaternion;
 public class ARCoreSource extends DeviceMotionDataSource {
   private static final String TAG = "ARCoreSource";
 
+  private final static Quaternion ROTATION_COMPENSATION =
+          new Quaternion((float)Math.sin(45 / (180 / Math.PI)), 0, 0, (float)Math.cos(45 / (180 / Math.PI)));
+
   private Scene mArScene;
   private Session mArSession;
   private SceneUpdateCallable mSceneUpdateCallback;
@@ -38,13 +41,10 @@ public class ARCoreSource extends DeviceMotionDataSource {
         camStart.getLocation().getX(),
         camStart.getLocation().getY(),
         camStart.getLocation().getZ());
-//    calcOrientationCorrection();
 
     this.mSceneUpdateCallback = sceneUpdateCallback;
   }
 
-  private final static Quaternion ROTATION_COMPENSATION =
-          new Quaternion((float)Math.sin(45 / (180 / Math.PI)), 0, 0, (float)Math.cos(45 / (180 / Math.PI)));
   @Override
   public void startAll() {
     if (mArSession != null) {
@@ -53,23 +53,6 @@ public class ARCoreSource extends DeviceMotionDataSource {
         // Taken from Xamarin iOS ARKit motion data source code.
         mArSession.resume();
         mArScene.setOnUpdateListener(new Scene.OnUpdateListener() {
-/*          @Override
-          public void onUpdate(FrameTime frameTime) {
-            if (mArSceneView.getArFrame().getCamera().getTrackingState() != TrackingState.TRACKING) return;
-
-            Vector3 v3Pos = mArSceneView.getScene().getCamera().getWorldPosition();
-            Quaternion qRotRaw = mArSceneView.getScene().getCamera().getWorldRotation();
-            // Order of multiplier and multiplicand is very important here!
-            Quaternion qRot = Quaternion.multiply(ROTATION_COMPENSATION, qRotRaw);
-
-            setRelativePosition(v3Pos.x, -v3Pos.z, v3Pos.y,
-                qRot.x, qRot.y, qRot.z, qRot.w, true
-            );
-
-            // Allow the caller to do something with the updated scene if it wants to
-            if (mSceneUpdateCallback != null)
-              mSceneUpdateCallback.onSceneUpdate(mArSceneView.getScene(), frameTime);
-          }*/
           @Override
           public void onUpdate(FrameTime frameTime) {
 //            if (mArSceneView.getArFrame().getCamera().getTrackingState() != TrackingState.TRACKING) return;
@@ -84,13 +67,10 @@ public class ARCoreSource extends DeviceMotionDataSource {
               return;
             }
 
-//            Vector3 v3Pos = mArSceneView.getScene().getCamera().getWorldPosition();
-//            Quaternion qRotRaw = mArSceneView.getScene().getCamera().getWorldRotation();
-            // Order of multiplier and multiplicand is very important here!
-//            Quaternion qRot = Quaternion.multiply(ROTATION_COMPENSATION, qRotRaw);
             Pose pose = frame.getCamera().getPose();
             float[] aryRot = pose.getRotationQuaternion();
             Quaternion qRotRaw = new Quaternion(aryRot[0], aryRot[1], aryRot[2], aryRot[3]);
+            // Order of multiplier and multiplicand is very important here!
             Quaternion qRot = Quaternion.multiply(ROTATION_COMPENSATION, qRotRaw);
             setRelativePosition(pose.tx(), -pose.tz(), pose.ty(),
                     qRot.x, qRot.y, qRot.z, qRot.w, true
@@ -111,24 +91,4 @@ public class ARCoreSource extends DeviceMotionDataSource {
   public void stopAll() {
     if (mArSession != null) mArSession.pause();
   }
-
-  // Get a correction factor for device based on current screen orientation
- /* private void calcOrientationCorrection() {
-    Display display = ((WindowManager) mArSceneView.getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-    int rotation = display.getRotation();
-    switch (rotation) {
-      case Surface.ROTATION_0:
-        mOrientationCorrection = new Quaternion(0, 0, (float)Math.sqrt(0.5), (float)Math.sqrt(0.5));
-        break;
-      case Surface.ROTATION_90:
-        mOrientationCorrection = new Quaternion(0, 0, 0, 1);
-        break;
-      case Surface.ROTATION_180:
-        mOrientationCorrection = new Quaternion(0, 0, -(float)Math.sqrt(0.5), (float)Math.sqrt(0.5));
-        break;
-      case Surface.ROTATION_270:
-        mOrientationCorrection = new Quaternion(0, 0, 1, 0);
-        break;
-    }
-  }*/
 }
